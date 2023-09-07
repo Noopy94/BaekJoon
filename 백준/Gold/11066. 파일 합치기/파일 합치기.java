ed2c15221f729955836ca4e53f4c85bd001c3077
Noopy94 <122508464+Noopy94@users.noreply.github.com>
2023-09-07 12:50:17 +0900

    [Gold III] Title: 파일 합치기, Time: 328 ms, Memory: 19172 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,45 +1,48 @@
-import java.util.*;
 import java.io.*;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    static int dp[][];
-    static int novel[];
-    static int sum[];
-    static int N;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int testCase = Integer.parseInt(br.readLine());
-
-        for (int t = 0; t < testCase; t++) {
-            N = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine(), " ");
-            dp = new int[N + 1][N + 1];
-            novel = new int[N + 1];
-            sum = new int[N + 1];
-
-            for (int i = 1; i <= N ; i++) {
-                novel[i] = Integer.parseInt(st.nextToken());
-                sum[i] = sum[i-1] + novel[i];
-            }
-
-            dp_algorithm();
-        }
-    }
-
-    public static void dp_algorithm() {
-        for (int n = 1; n <= N; n++) {
-            for (int i = 1; i + n <= N; i++) {
-                int j = i + n;
-                dp[i][j] = Integer.MAX_VALUE;
-
-                for (int x = i; x < j; x++) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i][x] + dp[x+1][j] + (sum[j] - sum[i-1]));
-                }
-            }
-        }
-
-        System.out.println(dp[1][N]);
-    }
+	
+	static int[] arr;
+	static int[] sum;
+	static int[][] dp;
+	static List<Integer> result;
+	
+	public static void solve(int K) {
+		
+		for (int i=1;i<=K;i++) {
+		for (int from=1;from+i<=K;from++) {
+				int to = from +i;
+				dp[from][to] = Integer.MAX_VALUE;
+		for (int divide = from; divide<to; divide++) dp[from][to] = Math.min(dp[from][to], dp[from][divide]+dp[divide+1][to]+sum[to]-sum[from-1]);
+		}
+		}
+		
+		result.add(dp[1][K]);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		result = new ArrayList<>();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc=0;tc<T;tc++) {
+			
+			int K = Integer.parseInt(br.readLine());
+			arr = new int[K+1];
+			sum = new int[K+1];
+			dp = new int[K+1][K+1];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i=1;i<=K;i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+				sum[i] = sum[i-1]+arr[i];
+			}
+			
+			solve(K);
+		}
+		
+		for (int c: result) System.out.println(c);
+	}
 }
